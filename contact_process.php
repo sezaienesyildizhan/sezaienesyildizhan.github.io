cce8b6c0858fe9664700b1dd8b5d2ed9f0547f07
Sezai Enes YILDIZHAN <sezaienesyildizhan@gmail.com>
2025-02-26 18:38:13 +0300

    contact process fixed

--- contact_process.php
+++ contact_process.php
@@ -1,37 +1,62 @@
 <?php
 
-    $to = "zmeyadev@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+    // Kullanıcıdan gelen verileri güvenli bir şekilde al
+    $to = "zmeyadev@gmail.com";  // Alıcı e-posta adresi
+    $from = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
+    $name = htmlspecialchars($_REQUEST['name'], ENT_QUOTES, 'UTF-8');
+    $subject = htmlspecialchars($_REQUEST['subject'], ENT_QUOTES, 'UTF-8');
+    $number = htmlspecialchars($_REQUEST['number'], ENT_QUOTES, 'UTF-8');
+    $cmessage = htmlspecialchars($_REQUEST['message'], ENT_QUOTES, 'UTF-8');
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    // E-posta başlıklarını oluştur
+    $headers = "From: " . $from . "\r\n";
+    $headers .= "Reply-To: " . $from . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-    $subject = "Müvekkil";
+    // Mail konusunu tanımla
+    $email_subject = "Yeni İletişim Formu Mesajı";
 
-    $logo = 'img/logo.png';
-    $link = '#';
+    // Mail içeriğini oluştur
+    $body = "<!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <title>Yeni Mesaj</title>
+    </head>
+    <body>
+        <table style='width: 100%; border-collapse: collapse;'>
+            <thead>
+                <tr>
+                    <td style='border:none; text-align:center;' colspan='2'>
+                        <img src='img/logo.png' alt='Furkan Hukuk' style='max-width:150px;'><br><br>
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style='border:none;'><strong>Adı:</strong> {$name}</td>
+                    <td style='border:none;'><strong>E-Posta:</strong> {$from}</td>
+                </tr>
+                <tr>
+                    <td style='border:none;'><strong>Konu:</strong> {$subject}</td>
+                </tr>
+                <tr>
+                    <td style='border:none;'><strong>Telefon Numarası:</strong> {$number}</td>
+                </tr>
+                <tr>
+                    <td colspan='2' style='border:none; padding-top:10px;'><strong>Mesaj:</strong><br>{$cmessage}</td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+    </html>";
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
-
-    $send = mail($to, $subject, $body, $headers);
+    // Mail gönder ve sonucu kontrol et
+    if (mail($to, $email_subject, $body, $headers)) {
+        echo "success";  // JavaScript tarafında başarıyı kontrol etmek için
+    } else {
+        echo "error";  // Hata durumunda
+    }
 
 ?>
